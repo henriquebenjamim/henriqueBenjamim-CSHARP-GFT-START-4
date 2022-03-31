bec3b90e7353266ff2dbb24e7f8018ba4b5e4fe5
henriquebenjamim <henriiqqq@gmail.com>
2022-03-31 17:15:59 -0300

    adding third question

---
+++ questoes/questaoTres/questaoTres/questaoTres/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace questaoTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vendinhaDasCriancas compra = new vendinhaDasCriancas("L", 2.0, 5.0);
+            vendinhaDasCriancas compra1 = new vendinhaDasCriancas("L", 3.0, 5.0);
+            vendinhaDasCriancas compra2 = new vendinhaDasCriancas("L", 6.0, 5.0);
+            vendinhaDasCriancas compra3 = new vendinhaDasCriancas("T", 2.0, 7.0);
+            vendinhaDasCriancas compra4 = new vendinhaDasCriancas("T", 3.0, 7.0);
+            vendinhaDasCriancas compra5 = new vendinhaDasCriancas("T", 6.0, 7.0);
+
+            Console.WriteLine(compra.calculandoDesconto());
+            Console.WriteLine(compra1.calculandoDesconto());
+            Console.WriteLine(compra2.calculandoDesconto());
+            Console.WriteLine(compra3.calculandoDesconto());
+            Console.WriteLine(compra4.calculandoDesconto());
+            Console.WriteLine(compra5.calculandoDesconto());
+
+
+
+
+        }
+    }
+}
---
+++ questoes/questaoTres/questaoTres/questaoTres/vendinhaDasCriancas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace questaoTres
+{
+    class vendinhaDasCriancas
+    {
+        public string TipoSuco { get; set; }
+        public double VolumeLitros { get; set; }
+        public double PrecoPorTotal { get; set; }
+
+        public vendinhaDasCriancas()
+        {
+        }
+
+        public vendinhaDasCriancas(string tipoSuco, double volumeLitros, double precoPorTotal)
+        {
+            TipoSuco = tipoSuco;
+            VolumeLitros = volumeLitros;
+            PrecoPorTotal = precoPorTotal;
+        }
+
+        public string calculandoDesconto()
+        {
+            //Limao - 3%/Litro(até 2 litros) -- 5%/Litro(Acima de 5 litros)
+            //Tamarindo - 4%/Litro(até 2 litros) -- 6%/Litro(Acima de 5 litros)
+            if (VolumeLitros <= 2.0)
+            {
+                // se o tipo do suco for limao
+                if (TipoSuco == "L")
+                {
+                    return "O cliente tem 3% de desconto para suco de limao até 2l e só pagará R$ " + (PrecoPorTotal * (0.03 * (PrecoPorTotal / VolumeLitros))).ToString("F2", CultureInfo.InvariantCulture);
+                }
+                // se for tamarindo
+                else if (TipoSuco == "T")
+                {
+                    return "O cliente tem 4% de desconto para suco de tamarindo até 2l e só pagará R$ " + (PrecoPorTotal * (0.04 * (PrecoPorTotal / VolumeLitros))).ToString("F2", CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    return "Este suco não existe.";
+                }
+
+            }
+            else if (VolumeLitros > 5.0)
+            {
+                // se o tipo do suco for limao
+                if (TipoSuco == "L")
+                {
+                    return "O cliente tem 5% de desconto/Litro para suco de limao acima de 5l e só pagará R$ " + (PrecoPorTotal * (0.05 * (PrecoPorTotal / VolumeLitros))).ToString("F2", CultureInfo.InvariantCulture);
+                }
+                // se for tamarindo
+                else if (TipoSuco == "T")
+                {
+                    return "O cliente tem 6% de desconto/Litro para suco de tamarindo acima de 5l e só pagará R$ " + (PrecoPorTotal * (0.06 * (PrecoPorTotal / VolumeLitros))).ToString("F2", CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    return "Este suco não existe.";
+                }
+            }
+            else
+            {
+                return "não existe desconto nessas condicoes.";
+            }
+        }
+    }
+}
