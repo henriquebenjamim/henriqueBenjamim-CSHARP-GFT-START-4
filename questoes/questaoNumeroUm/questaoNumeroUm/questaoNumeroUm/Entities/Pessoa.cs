6ea890c8ebb5d5e5acf285cdaa03e0f021638e9a
henriquebenjamim <henriiqqq@gmail.com>
2022-03-31 17:33:33 -0300

    Organizing folders and firstQuestion problems

---
+++ questoes/questaoNumeroUm/questaoNumeroUm/questaoNumeroUm/Entities/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace questaoNumeroUm.Entities
+{
+    internal class Pessoa
+    {
+        private string Nome { get; set; }
+        private DateTime DataDeNascimento { get; set; }
+        private double Altura { get; set; }
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome, DateTime dataDeNascimento, double altura)
+        {
+            Nome = nome;
+            DataDeNascimento = dataDeNascimento;
+            Altura = altura;
+        }
+
+        public string calcularIdade()
+        {
+            // subtraindo data de agora com a data de nascimento.
+            int age = (int)((DateTime.Now - DataDeNascimento).TotalDays / 365.242199);
+            return "Tem " + age + " anos.";
+            
+        }
+        public override string ToString()
+        {
+            return "A pessoa: " + Nome
+                + " nasceu em " + DataDeNascimento.ToString("dd/MM/yyyy")
+                + " tem de altura: " + Altura.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ questoes/questaoNumeroUm/questaoNumeroUm/questaoNumeroUm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using questaoNumeroUm.Entities;
+
+namespace questaoNumeroUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Data de nascimento: ");
+            DateTime dataNasc = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Pessoa pessoa = new Pessoa(nome, dataNasc, altura);
+
+            Console.WriteLine(pessoa.ToString());
+            Console.WriteLine(pessoa.calcularIdade());
+        }
+    }
+}
