ea4d0df8e8c9ceb59072689ab2415257aa169a6b
henriquebenjamim <henriiqqq@gmail.com>
2022-03-31 16:10:47 -0300

    Adding repo folder

---
+++ questaoUm/questaoUm/questaoUm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace questaoUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valor = Console.ReadLine().Split();
+            int A = int.Parse(valor[0]);
+            int B = int.Parse(valor[1]);
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+        }
+    }
+}
