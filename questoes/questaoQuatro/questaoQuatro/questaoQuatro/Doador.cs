4b58654fc52be91e5f474560b3e360e29ade67e9
henriquebenjamim <henriiqqq@gmail.com>
2022-03-31 17:22:15 -0300

    Adding fourth question

---
+++ questoes/questaoQuatro/questaoQuatro/questaoQuatro/Doador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questaoQuatro
+{
+    class Doador
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Doador()
+        { 
+        }
+
+        public Doador(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public string validandoIdadeDoador()
+        {
+            if(Idade >= 18 && Idade < 67)
+            {
+                return "Você pode doar sangue.";
+            }
+            else
+            {
+                return "Infelizmente você não pode doar sangue, pois não atende o intervalo de idade (entre 18 e 67).";
+            }
+        }
+    }
+}
---
+++ questoes/questaoQuatro/questaoQuatro/questaoQuatro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace questaoQuatro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Informe sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            Doador pessoaDoadora = new Doador(nome, idade);
+            Console.WriteLine(pessoaDoadora.validandoIdadeDoador());
+        }
+    }
+}
